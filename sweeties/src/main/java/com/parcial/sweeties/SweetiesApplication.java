d48d6d5c61d9570e143caa0453bb956ec215e650
LorenzoPorrasDuque <lorenzo.porras.duque@gmail.com>
2023-09-02 08:07:53 -0500

    parcial

---
+++ sweeties/src/main/java/com/parcial/sweeties/SweetiesApplication.java
@@ -0,0 +1,13 @@
+package com.parcial.sweeties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetiesApplication.class, args);
+	}
+
+}
---
+++ sweeties/src/test/java/com/parcial/sweeties/SweetiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial.sweeties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
